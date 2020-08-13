89ecb7b50306dede1e1a1cd0946c07d9385b0041
theericwright <theericwright@aol.com>
2020-08-13 19:58:43 -0400

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Header() {
+return (
+  <header>
+    <h1 style={{ fontSize: "25px", marginBottom: "15px" }}>Simple Todo App</h1>
+    <p style={{ fontSize: "19px" }}>
+      Please add to-dos item(s) through the input field
+    </p>
+  </header>
+)
+}
+export default Header
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+//component file
+import TodoContainer from "./components/TodoContainer"
+import "./App.css"
+
+
+
+ReactDOM.render(<TodoContainer />, document.getElementById("root"))
